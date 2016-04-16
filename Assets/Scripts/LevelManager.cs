c42377c1f8a56c20bc1e1fcbda7b4dff67a18a94
Avishaan <avishaan@codeHatcher.com>
2016-04-16 14:45:59 -0700

    change scene on spacebar tap

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -7,12 +7,18 @@ public class LevelManager : MonoBehaviour {
 
   // Use this for initialization
   void Start () {
-    // Load Scene
-    SceneManager.LoadScene("Main");
+
   }
 
   // Update is called once per frame
   void Update () {
+    if (Input.GetKeyDown(KeyCode.Space)){
+      LoadNextScene();
+    }
+  }
 
+  void LoadNextScene() {
+    // Load Scene
+    SceneManager.LoadScene("Main");
   }
 }
